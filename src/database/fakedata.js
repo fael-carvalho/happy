6b40b39afd37a0819cef314beb4e18264ac20f4b
Rafael Carvalho <rafael.carv28@gmail.com>
2020-10-15 13:31:35 -0300

    nlw day four

---
+++ src/database/fakedata.js
@@ -0,0 +1,37 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-27.222633", 
+        lng: "-49.6455874",
+        name: "Lar das meninas",
+        descripition:  "Prestamos assistência de 86 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1602389569471-5df5bde61968?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1598539961915-040bb3be3f69?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "1",
+
+
+    },
+
+
+    {
+        id: 2,
+        lat: "-27.222633", 
+        lng: "-49.6555874",
+        name: "Lar dos meninos",
+        descripition:  "Prestamos assistência de 86 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1602389569471-5df5bde61968?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+            "https://images.unsplash.com/photo-1598539961915-040bb3be3f69?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "0"
+
+    }
+]
